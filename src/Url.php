6cca361aabffdecd4a66bbb9598e0dcbc307cd62
Parikshit Mishra <pm00352674@techmahindra.com>
2016-01-11 11:10:24 +0100

    Url class with method to get values from it

--- src/Url.php
+++ src/Url.php
@@ -1,10 +1,71 @@
 <?php namespace job\nl;
  
 class Url {
+  
+  private $requestUrl;
  
-  public function hasCheese($bool = true)
+  public function __construct($url="")
   {
-    return $bool;
+	$this->requestUrl = $this->doCleanUrl($url);		
+  }
+  
+	/**
+	 * @author Parikshit
+	 * @version v1.0
+	 * @project testnl
+	 * @since 10/01/2016
+	 */
+  public function getProtocol()
+  {
+    return parse_url($this->requestUrl, PHP_URL_SCHEME);
+  }
+
+  /**
+	 * @author Parikshit
+	 * @version v1.0
+	 * @project testnl
+	 * @since 10/01/2016
+	 */  
+  public function getPath()
+  {
+    return parse_url($this->requestUrl, PHP_URL_PATH);
+  }
+    
+	/**
+	 * @author Parikshit
+	 * @version v1.0
+	 * @project testnl
+	 * @since 10/01/2016
+	 */
+  public function getHostName()
+  {
+    return parse_url($this->requestUrl, PHP_URL_HOST);
+  }
+    
+  /**
+	 * @author Parikshit
+	 * @version v1.0
+	 * @project testnl
+	 * @since 10/01/2016
+	 */	
+  public function __toString()
+  {
+     return $this->requestUrl;
+  }
+  
+    /**
+	 * @author Parikshit
+	 * @version v1.0
+	 * @project testnl
+	 * @since 10/01/2016
+	 */
+  public function doCleanUrl($url="")
+  {
+	$url = preg_replace("/\.\//","",$url);
+	$url = preg_replace("/\..\//","",$url);
+	$url = trim($url,'.');
+    
+	return $url;
   }
  
 }
--- tests/UrlTest.php
+++ tests/UrlTest.php
@@ -4,11 +4,82 @@ use job\nl\Url;
  
 class UrlTest extends PHPUnit_Framework_TestCase {
  
-  public function testUrlHasCheese()
+  /**
+	 * @author Parikshit
+	 * @version v1.0
+	 * @project testnl
+	 * @since 10/01/2016
+	 */
+  public function testGetProtocol()
   {
-    $nacho = new Url;
-    $this->assertTrue($nacho->hasCheese());
-	$this->assertFalse($nacho->hasCheese(false));
+	$url = 'https://www.google.com/class/abc.php';
+    $objUrl = new Url($url);
+    $this->assertSame('https',$objUrl->getProtocol());		
   }
- 
+  
+ /**
+	 * @author Parikshit
+	 * @version v1.0
+	 * @project testnl
+	 * @since 10/01/2016
+	 */
+  public function testGetPath()
+  {
+	$url = 'https://www.google.com/class/abc.php?path=';
+    $objUrl = new Url($url);
+    $this->assertSame('/class/abc.php',$objUrl->getPath());		
+  }
+  /**
+	 * @author Parikshit
+	 * @version v1.0
+	 * @project testnl
+	 * @since 10/01/2016
+	*/
+  public function testGetHostName()
+  {
+	$url = 'https://www.google.com/class/abc.php';
+    $objUrl = new Url($url);
+    $this->assertSame('www.google.com',$objUrl->getHostName());		
+  }	
+  
+  /**
+	 * @author Parikshit
+	 * @version v1.0
+	 * @project testnl
+	 * @since 10/01/2016
+	 */
+	 
+  public function testGetString()
+  {
+	$url = 'https://www.google.com/class/abc.php';
+    $objUrl = new Url($url);
+	$this->assertEquals($url,$objUrl);		
+  }	
+  
+  /**
+	 * @author Parikshit
+	 * @version v1.0
+	 * @project testnl
+	 * @since 10/01/2016
+	 */
+  public function testShortUrl()
+  {
+	$url = './../class/abc.php';
+    $objUrl = new Url($url);	
+	$this->assertSame(null,$objUrl->getHostName());		
+  }	
+  
+  	/**
+	 * @author Parikshit
+	 * @version v1.0
+	 * @project testnl
+	 * @since 10/01/2016
+	 */
+  public function testDoCleanUrl($url="")
+  {
+	$url = './../class/abc.php';	
+    $objUrl = new Url();	
+	$this->assertSame('class/abc.php',$objUrl->doCleanUrl($url));		
+  }
+  
 }
